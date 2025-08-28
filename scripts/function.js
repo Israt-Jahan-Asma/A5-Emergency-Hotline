1d4ad6a13c5110ffa6eb785c1499b4fe6ee8bf35
Israt-Jahan-Asma <isratjahanasma786@gmail.com>
2025-08-29 02:06:47 +0600

    alert setup done

--- scripts/function.js
+++ scripts/function.js
@@ -1,3 +1,5 @@
+// Clicking on the 💗 heart icon of any card will increase the count in the Navbar
+
 const heartCountSpan = document.getElementById('heart-count');
 const heartButtons = document.querySelectorAll('.heart-button');
 
@@ -7,4 +9,17 @@ for (let i = 0; i < heartButtons.length; i++) {
         count++;
         heartCountSpan.innerText = count;
     });
+}
+
+// Show an alert with a message including the service name and number
+const callButtons = document.querySelectorAll('.call-button')
+for (let i= 0; i< callButtons.length ; i++){
+    callButtons[i].addEventListener('click', function(){
+      let card = callButtons[i].closest('.card');
+
+      let serviceName = card.querySelector('.service-name').innerText;
+      let serviceNumber = card.querySelector('.service-number').innerText;
+
+      alert( "Calling " + serviceName + ' ' + serviceNumber);
+    })
 }
