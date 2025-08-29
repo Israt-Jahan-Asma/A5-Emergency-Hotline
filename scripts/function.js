9b1e2286339de238fd031d693654e9b0446e9cb5
Israt-Jahan-Asma <isratjahanasma786@gmail.com>
2025-08-29 16:06:27 +0600

    coin system setup done

--- scripts/function.js
+++ scripts/function.js
@@ -11,15 +11,32 @@ for (let i = 0; i < heartButtons.length; i++) {
     });
 }
 
-// Show an alert with a message including the service name and number
+
 const callButtons = document.querySelectorAll('.call-button')
 for (let i= 0; i< callButtons.length ; i++){
     callButtons[i].addEventListener('click', function(){
-      let card = callButtons[i].closest('.card');
 
+        // Each call will cut 20 coins. Reduce Coin after each click.
+
+      const coinSpan = document.getElementById('coin-count');
+       let coins = parseInt(coinSpan.innerText);
+        if (coins < 20) {
+            alert("Not enough coins to make a call!");
+            return;
+        }
+        coins -= 20;
+        coinSpan.innerText = coins;
+ 
+// Show an alert with a message including the service name and number
+      let card = callButtons[i].closest('.card');
       let serviceName = card.querySelector('.service-name').innerText;
       let serviceNumber = card.querySelector('.service-number').innerText;
 
       alert( "Calling " + serviceName + ' ' + serviceNumber);
+
     })
 }
+
+
+
+
